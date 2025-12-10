b770943936fc5828217db551342ca90958afce40
brito101 <rodrigobrito101@hotmail.com>
2025-12-10 17:52:38 -0300

    feat: add tables module

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { ProductsApi } from './api/products/index.ts';
+import { AuthApi } from './api/auth/index.ts';
+import { LmsApi } from './api/lms/index.ts';
 import { Core } from './core/core.ts';
 import { logger } from './core/middleware/logger.ts';
 
@@ -6,7 +7,8 @@ const core = new Core();
 
 core.router.use([logger]);
 
-new ProductsApi(core).init();
+new AuthApi(core).init();
+new LmsApi(core).init();
 
 core.router.get('/', (req, res) => {
   res.status(200).json('ola');
